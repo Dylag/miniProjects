1c325e3a782e8b1d0d547d09543922ec74b6c164
Stepan Dementev <80048504+Dylag@users.noreply.github.com>
2023-08-29 21:23:46 +0300

    [todoList] add deleting

--- todoList/script.js
+++ todoList/script.js
@@ -2,8 +2,23 @@ let userInput  = document.getElementById("userInput")
 let list = document.getElementById('list')
 
 
+
 function addToList(){
     let newLi = document.createElement('li')
-    newLi.textContent=userInput.value
+
+    let newLabel = document.createElement('label')
+    newLabel.textContent = userInput.value
+
+    let newBtn = document.createElement('button')
+    newBtn.textContent = 'delete'
+    newBtn.addEventListener('click',()=>removeElement(newLi))
+
+    newLi.appendChild(newLabel)
+    newLi.appendChild(newBtn)
+
     list.appendChild(newLi)
+}
+
+function removeElement(element){
+    element.remove()
 }
