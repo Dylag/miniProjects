83ceabdc382b9014878970d61d9b2b7d62f094d1
Stepan Dementev <80048504+Dylag@users.noreply.github.com>
2023-08-29 23:53:05 +0300

    [todo-web-java-rest] create

---
+++ todolist-web-java-rest/src/main/java/com/example/todolistwebjavarest/TodolistWebJavaRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.todolistwebjavarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistWebJavaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistWebJavaRestApplication.class, args);
+	}
+
+}
