126bc96f26ae9171125ff861881c61bfded93b96
ssspamqe <80048504+Dylag@users.noreply.github.com>
2023-09-08 11:54:06 +0300

    [websocket] initial

---
+++ websocket/src/test/java/com/sspamqe/websocket/WebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sspamqe.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
